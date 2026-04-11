5ef71eeaaf1e8946c795227bead673723d1ff0f9
Your Nameaymansiddiqui2006 <aymansiddiqui2006@gmail.com>
2026-04-11 17:16:04 +0530

    fix: enhance CORS configuration with allowed origins and error handling

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,48 @@
-import express from 'express'
-import authRoute from './routes/authRoute.routes.js'
-import incomeRoute from './routes/incomeRoute.routes.js'
-import expenseRoute from './routes/expense.route.js'
-import dashboardRoute from './routes/dashboard.route.js'
-import cors from 'cors'
-import path from 'path'
+import express from "express";
+import authRoute from "./routes/authRoute.routes.js";
+import incomeRoute from "./routes/incomeRoute.routes.js";
+import expenseRoute from "./routes/expense.route.js";
+import dashboardRoute from "./routes/dashboard.route.js";
+import cors from "cors";
+import path from "path";
 
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app=express()
+const app = express();
 
-app.use(cors({
-  origin: "*",
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://expense-tracker-app-4dpz.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS blocked"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api/v1/auth", authRoute)
-app.use("/api/v1/income",incomeRoute)
-app.use("/api/v1/expense",expenseRoute)
-app.use("/api/v1/dashboard",dashboardRoute)
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/income", incomeRoute);
+app.use("/api/v1/expense", expenseRoute);
+app.use("/api/v1/dashboard", dashboardRoute);
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
@@ -33,4 +51,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-export default app
+export default app;
